12c41ce21d64df27ffb552713d037e3e895e2e82
Mateusz <mateuszhotowy@gmail.com>
2018-03-17 18:52:43 +0100

    Changed db properties from CREATE to UPDATE, Disabled data.sql

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_role (id,role,username) VALUES (1,'ADMIN','admin');
-INSERT INTO user_role (id,role,username) VALUES (2,'user','user');
---
+++ src/main/resources/dataOff.sql
@@ -0,0 +1,2 @@
+--INSERT INTO user_role (id,role,username) VALUES (1,'ADMIN','admin');
+--INSERT INTO user_role (id,role,username) VALUES (2,'user','user');
